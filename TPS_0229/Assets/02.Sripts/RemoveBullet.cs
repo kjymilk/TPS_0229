6feb92c75f049efa080d3c31bfa394eb068fa36c
kjymilk <beyondmydream229@gmail.com>
2023-06-01 19:52:54 +0900

    0601

--- TPS_0229/Assets/02.Sripts/RemoveBullet.cs
+++ TPS_0229/Assets/02.Sripts/RemoveBullet.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
-   // 스파크 파티클 프리팹을 연결할 변수
-public GameObject sparkEffect;
+    // 스파크 파티클 프리팹을 연결할 변수
+    public GameObject sparkEffect;
     // 충돌이 시작할 때 발생하는 이벤트
     void OnCollisionEnter(Collision coll)
     {
@@ -16,8 +16,11 @@ public GameObject sparkEffect;
             ContactPoint cp = coll.GetContact(0);
             // 충돌한 총알의 법선 벡터를 쿼터니언 타입으로 변환
             Quaternion rot = Quaternion.LookRotation(-cp.normal);
+            
             // 스파크 파티클을 동적으로 생성
-/*            Instantiate(sparkEffect, cp.point, rot);*/
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+            // 일정 시간이 지난 후 스파크 파티클을 삭제
+            Destroy(spark, 0.5f);
             // 충돌한 게임오브젝트 삭제
             Destroy(coll.gameObject);
         }
